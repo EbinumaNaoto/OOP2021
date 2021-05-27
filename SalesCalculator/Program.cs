23291d16f779957ae1e57e4aa7e6cf405f43b03e
海老沼 尚人 <32063@ojs.ac.jp>
2021-05-27 15:50:45 +0900

    P70 リスト2-22

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -10,29 +10,12 @@ namespace SalesCalculator {
     class Program {
 
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(ReadSales("Sales.csv"));
+            var sales = new SalesCounter("Sales.csv");
 
-            Dictionary<string, int> amountPerStore =  sales.GetPerStoreSales();
+            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string, int> obj in amountPerStore) {
                 Console.WriteLine("{0} {1}",obj.Key, obj.Value);
             }
-        }
-
-        // List2-15
-        // 売上データを読み込み、Saleオブジェクトのリストを返す
-        private static List<Sale> ReadSales(string filePath) {
-            List<Sale> sales = new List<Sale>();
-            string[] lines = File.ReadAllLines(filePath);
-            foreach (string line in lines) {
-                string[] items = line.Split(',');
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);
-            }
-            return sales;
         }
     }
 }
