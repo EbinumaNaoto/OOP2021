a17d2a05d22b59e099275f3544714156fe249388
海老沼 尚人 <32063@ojs.ac.jp>
2021-05-28 14:05:48 +0900

    P77 リスト2-2＿完成版

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -12,8 +12,8 @@ namespace SalesCalculator {
         static void Main(string[] args) {
             var sales = new SalesCounter("Sales.csv");
 
-            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountPerStore) {
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
                 Console.WriteLine("{0} {1}",obj.Key, obj.Value);
             }
         }
