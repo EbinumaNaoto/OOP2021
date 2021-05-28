47b59fd027c893debc9628787e7451b727b6a013
海老沼 尚人 <32063@ojs.ac.jp>
2021-05-28 15:32:14 +0900

    P79 演習問題１ 完成

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Program {
+
         static void Main(string[] args) {
+            //2.1.3
+            var songs = new Song[3];
+            songs[0] = new Song("光、再考", "amazarashi", 339);
+            songs[1] = new Song("つじつま合わせに生まれた僕等", "amazarashi", 340);
+            songs[2] = new Song("少年少女", "amazarashi", 345);
+            PrintSongs(songs);
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine($"アーティスト名: {song.ArtistName}");
+                Console.WriteLine($"タイトル: {song.Title}");
+                Console.WriteLine("演奏時間: " + (song.Length / 60) + ":" + (song.Length % 60));
+                Console.WriteLine();
+            }
         }
     }
 }
--- Exercise1/Song.cs
+++ Exercise1/Song.cs
@@ -5,6 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise1 {
+
     class Song {
+        /*
+        //フィールド
+        private string title;       //歌のタイトル
+        private string artistName;  //アーティスト名
+        private int length;         //演奏時間、単位は秒
+        */
+
+        //プロパティ
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        //引数付きコンストラクタ
+        public Song(string title,string artistName,int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+        }
     }
 }
