c24700dc6a78a2d98c6a862835ea667ed4106c46
海老沼 尚人 <32063@ojs.ac.jp>
2021-06-01 11:01:04 +0900

    P79 演習問題３ 完成

--- Exercize3/Program.cs
+++ Exercize3/Program.cs
@@ -8,7 +8,13 @@ namespace Exercize3 {
     class Program {
 
         static void Main(string[] args) {
+            var sales = new SalesCounter("Sales.csv");
 
+            //var amountPerStore = sales.GetPerStoreSales(); //店舗別売り上げを求める
+            var amountPerCategory = sales.GetPerCategorySales(); //商品カテゴリ別売り上げを求める
+            foreach (var obj in amountPerCategory) {
+                Console.WriteLine("{0} {1:#,0}円", obj.Key, obj.Value);
+            }
         }
     }
 }
---
+++ Exercize3/SalesCounter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercize3 {
+    //売上集計クラス
+    class SalesCounter {
+
+        private IEnumerable<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
+        }
+
+        // List2-15
+        // 売上データを読み込み、Saleオブジェクトのリストを返す
+        private IEnumerable<Sale> ReadSales(string filePath) {
+            var sales = new List<Sale>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
+        //List 2-17
+        //店舗別売上
+        public IDictionary<string, int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName)) {
+                    //既にコレクションに店舗が設定されている
+                    dict[sale.ShopName] += sale.Amount;
+                } else {
+                    //コレクションへ店舗を登録
+                    dict[sale.ShopName] = sale.Amount;
+                }
+            }
+            return dict;
+        }
+
+        //Exercize3
+        //商品カテゴリ別売上
+        public IDictionary<string, int> GetPerCategorySales() {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.ProductCategory)) {
+                    //既にコレクションに店舗が設定されている
+                    dict[sale.ProductCategory] += sale.Amount;
+                } else {
+                    //コレクションへ店舗を登録
+                    dict[sale.ProductCategory] = sale.Amount;
+                }
+            }
+            return dict;
+        }
+
+    }
+}
