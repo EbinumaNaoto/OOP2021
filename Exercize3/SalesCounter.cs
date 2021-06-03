30e4c7830fd4b730fa5d879496918ae474d635d1
海老沼 尚人 <32063@ojs.ac.jp>
2021-06-03 15:54:38 +0900

    P99 演習問題１完成

--- Exercize3/SalesCounter.cs
+++ Exercize3/SalesCounter.cs
@@ -55,10 +55,10 @@ namespace Exercize3 {
             var dict = new Dictionary<string, int>();
             foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ProductCategory)) {
-                    //既にコレクションに店舗が設定されている
+                    //既にコレクションに商品カテゴリが設定されている
                     dict[sale.ProductCategory] += sale.Amount;
                 } else {
-                    //コレクションへ店舗を登録
+                    //コレクションへ商品カテゴリを登録
                     dict[sale.ProductCategory] = sale.Amount;
                 }
             }
---
+++ Section01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise1_4(numbers);
+
+        }
+
+        private static void Exercise1_1(List<int> numbers) {
+            var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+
+            if (exists)
+                Console.WriteLine("存在しています");
+            else
+                Console.WriteLine("存在していません");
+        }
+
+        private static void Exercise1_2(List<int> numbers) {
+            numbers.ForEach(n => Console.Write("{0}, ",n/2.0));
+            Console.WriteLine();
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+            var query = numbers.Where(n => n >= 50);
+            foreach (var n in query) {
+                Console.Write("{0}, ",n);
+            }
+            Console.WriteLine();
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+            var query = numbers.Select(n => n * 2).ToList();
+            query.ForEach(n => Console.Write("{0}, ",n));
+        }
+
+    }
+}
