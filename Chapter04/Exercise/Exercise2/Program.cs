b99ae34f2346dc88cf6390a3e8b25cb7d091d1e8
海老沼 尚人 <32063@ojs.ac.jp>
2021-06-10 15:43:56 +0900

    P133 演習問題１ 完成 P134 演習問題２ 4.2.1完成

---
+++ Chapter04/Exercise/Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise1;
+
+namespace Exercise2 {
+
+    class Program {
+
+        static void Main(string[] args) {
+            //4.2.1
+            var yearMonth = new YearMonth[] {
+                new YearMonth(1999, 2),
+                new YearMonth(2005, 4),
+                new YearMonth(1983, 11),
+                new YearMonth(2015,7),
+                new YearMonth(2020,12),
+            };
+
+            //4.2.2
+        }
+    }
+}
