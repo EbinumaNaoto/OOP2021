4d11f9e6869312c1717553ff7229dac947177efa
海老沼 尚人 <32063@ojs.ac.jp>
2021-07-01 15:57:10 +0900

    P202 演習問題 7-1完成

---
+++ Chapter06/Exercise/Exercise1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35, };
+
+            #region テスト用ドライバ
+            //6.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("------");
+
+            //6.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("------");
+
+            //6.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("------");
+
+            //6.1.4
+            Exercise1_4(numbers);
+            Console.WriteLine("------");
+
+            //6.1.5
+            Exercise1_5(numbers);
+            #endregion
+        }
+
+        private static void Exercise1_1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+            var skip = numbers.Length - 2;
+            foreach (var number in numbers.Skip(skip))
+                Console.Write(number + " ");
+            Console.WriteLine();
+        }
+
+        private static void Exercise1_3(int[] numbers) {
+            numbers.Select(i => i.ToString()).ToList().ForEach(s => Console.Write(s+" "));
+            Console.WriteLine();
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+            numbers.OrderBy(i => i).Take(3).ToList().ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+            Console.WriteLine(numbers.Distinct().Count(i => i > 10));
+        }
+    }
+}
---
+++ Chapter07/Exercise1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+        }
+
+        private static void Exercise1_1(string text) {
+            var dict = new Dictionary<Char ,int>();
+            foreach (var c in text.ToUpper().OrderBy(c => c)) {
+                //大文字小文字を区別しないため全て大文字に変換して処理
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c))
+                        dict[c]++;
+                    else
+                        dict[c] = 1;
+                }
+            }
+            
+            foreach (var item in dict) {
+                Console.WriteLine($"{item.Key} ,{item.Value}");
+            }
+            
+        }
+
+        private static void Exercise1_2(string text) {
+            var dict = new SortedDictionary<Char, int>();
+            foreach (var c in text.ToUpper()) {
+                //大文字小文字を区別しないため全て大文字に変換して処理
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c))
+                        dict[c]++;
+                    else
+                        dict[c] = 1;
+                }
+            }
+
+            foreach (var item in dict) {
+                Console.WriteLine($"{item.Key} ,{item.Value}");
+            }
+
+        }
+    }
+}
