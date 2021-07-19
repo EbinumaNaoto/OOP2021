1ec2aea0fa20b204f29966f94d714341127d444a
尚人 海老沼 <32063@ojs.ac.jp>
2021-07-19 15:56:57 +0900

    夏休み前

---
+++ Chapter09/Exercise2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Exercise2 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonChangeFile = new System.Windows.Forms.Button();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonTextFileAdd = new System.Windows.Forms.Button();
+            this.openFileDialogAdd = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonOpenFile.Location = new System.Drawing.Point(137, 9);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(129, 37);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "開く...";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(13, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "問題9.2";
+            // 
+            // buttonChangeFile
+            // 
+            this.buttonChangeFile.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonChangeFile.Location = new System.Drawing.Point(137, 52);
+            this.buttonChangeFile.Name = "buttonChangeFile";
+            this.buttonChangeFile.Size = new System.Drawing.Size(218, 37);
+            this.buttonChangeFile.TabIndex = 0;
+            this.buttonChangeFile.Text = "変更ファイルの保存";
+            this.buttonChangeFile.UseVisualStyleBackColor = true;
+            this.buttonChangeFile.Click += new System.EventHandler(this.buttonChangeFile_Click);
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonChange.Location = new System.Drawing.Point(137, 95);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(159, 37);
+            this.buttonChange.TabIndex = 0;
+            this.buttonChange.Text = "行番号追加";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(13, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "問題9.3";
+            // 
+            // buttonTextFileAdd
+            // 
+            this.buttonTextFileAdd.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonTextFileAdd.Location = new System.Drawing.Point(137, 144);
+            this.buttonTextFileAdd.Name = "buttonTextFileAdd";
+            this.buttonTextFileAdd.Size = new System.Drawing.Size(218, 37);
+            this.buttonTextFileAdd.TabIndex = 0;
+            this.buttonTextFileAdd.Text = "テキストファイル追加";
+            this.buttonTextFileAdd.UseVisualStyleBackColor = true;
+            this.buttonTextFileAdd.Click += new System.EventHandler(this.buttonTextFileAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonTextFileAdd);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.buttonChangeFile);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonChangeFile;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonTextFileAdd;
+        private System.Windows.Forms.OpenFileDialog openFileDialogAdd;
+    }
+}
+
---
+++ Chapter09/Exercise2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise2 {
+    public partial class Form1 : Form {
+
+        string inputPath = ""; //変換元ファイル(読み込むファイル)
+        string outputPath = ""; //変換先ファイル
+        string[] lines = null;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        //変換元のファイルを指定
+        private void buttonOpenFile_Click(object sender, EventArgs e) {
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                inputPath = openFileDialog.FileName;
+            }
+        }
+
+        //変換したファイルの保存
+        private void buttonChangeFile_Click(object sender, EventArgs e) {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK) {
+                outputPath = saveFileDialog.FileName;
+                File.WriteAllLines(outputPath,lines);
+            }
+        }
+
+        //行番号を追加する処理
+        private void buttonChange_Click(object sender, EventArgs e) {
+            lines = File.ReadLines(inputPath, Encoding.UTF8).Select((s, index) => string.Format($"{index + 1}: {s}")).ToArray();
+        }
+
+        //テキストファイルの挿入する処理
+        private void buttonTextFileAdd_Click(object sender, EventArgs e) {
+            if (openFileDialogAdd.ShowDialog() == DialogResult.OK) {
+                using (var writer = new StreamWriter(openFileDialogAdd.FileName, append: true)) {
+                    
+                }
+            }
+
+        }
+    }
+}
