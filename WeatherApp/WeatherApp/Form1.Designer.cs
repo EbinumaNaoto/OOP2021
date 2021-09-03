417b8b7339ababc74c1fa1193cf8f8e7d3901348
尚人 海老沼 <32063@ojs.ac.jp>
2021-09-03 15:49:54 +0900

    演習２ 完成

--- WeatherApp/WeatherApp/Form1.Designer.cs
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -24,13 +24,70 @@ namespace WeatherApp {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.areaInputLabel = new System.Windows.Forms.Label();
+            this.weatherDisplayButton = new System.Windows.Forms.Button();
+            this.weatherOutputTextbox = new System.Windows.Forms.TextBox();
+            this.areaInputComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // areaInputLabel
+            // 
+            this.areaInputLabel.AutoSize = true;
+            this.areaInputLabel.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.areaInputLabel.Location = new System.Drawing.Point(13, 13);
+            this.areaInputLabel.Name = "areaInputLabel";
+            this.areaInputLabel.Size = new System.Drawing.Size(89, 17);
+            this.areaInputLabel.TabIndex = 0;
+            this.areaInputLabel.Text = "地域を入力";
+            // 
+            // weatherDisplayButton
+            // 
+            this.weatherDisplayButton.Location = new System.Drawing.Point(302, 13);
+            this.weatherDisplayButton.Name = "weatherDisplayButton";
+            this.weatherDisplayButton.Size = new System.Drawing.Size(89, 31);
+            this.weatherDisplayButton.TabIndex = 1;
+            this.weatherDisplayButton.Text = "天気表示\r\n";
+            this.weatherDisplayButton.UseVisualStyleBackColor = true;
+            this.weatherDisplayButton.Click += new System.EventHandler(this.weatherDisplayButton_Click);
+            // 
+            // weatherOutputTextbox
+            // 
+            this.weatherOutputTextbox.Location = new System.Drawing.Point(16, 50);
+            this.weatherOutputTextbox.Multiline = true;
+            this.weatherOutputTextbox.Name = "weatherOutputTextbox";
+            this.weatherOutputTextbox.Size = new System.Drawing.Size(772, 388);
+            this.weatherOutputTextbox.TabIndex = 2;
+            // 
+            // areaInputComboBox
+            // 
+            this.areaInputComboBox.FormattingEnabled = true;
+            this.areaInputComboBox.Location = new System.Drawing.Point(108, 13);
+            this.areaInputComboBox.Name = "areaInputComboBox";
+            this.areaInputComboBox.Size = new System.Drawing.Size(168, 23);
+            this.areaInputComboBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.areaInputComboBox);
+            this.Controls.Add(this.weatherOutputTextbox);
+            this.Controls.Add(this.weatherDisplayButton);
+            this.Controls.Add(this.areaInputLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label areaInputLabel;
+        private System.Windows.Forms.Button weatherDisplayButton;
+        private System.Windows.Forms.TextBox weatherOutputTextbox;
+        private System.Windows.Forms.ComboBox areaInputComboBox;
     }
 }
 
--- WeatherApp/WeatherApp/Form1.cs
+++ WeatherApp/WeatherApp/Form1.cs
@@ -4,14 +4,70 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace WeatherApp {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
         }
+
+        private void weatherDisplayButton_Click(object sender, EventArgs e) {
+            weatherOutputTextbox.Text = "";
+            var results = GetWeatherReportFromYahoo(areaNameToAreaCode(areaInputComboBox.Text));
+            foreach (var s in results) {
+                weatherOutputTextbox.Text += s+"\r\n";
+            }
+        }
+
+        //検索
+        private int areaNameToAreaCode(string areaName) {
+            var areaCode = new Dictionary<string, int> {
+                                                        {"東京",4410},
+                                                        {"大島",4420},
+                                                        {"八丈島",4430},
+                                                        {"父島",4440},
+                                                        {"横浜",4610},
+                                                        {"小田原",4620},
+                                                        {"さいたま",4310},
+                                                        {"熊谷",4320},
+                                                        {"秩父",4330},
+                                                        {"千葉",4510},
+                                                        {"銚子",4520},
+                                                        {"館山",4530},
+                                                        {"水戸",4010},
+                                                        {"土浦",4020},
+                                                        {"宇都宮",4110},
+                                                        {"大田原",4120},
+                                                        {"前橋",4210},
+                                                        {"みなかみ",4220},
+                                                        {"甲府",4910},
+                                                        {"河口湖",4920}, };
+
+            return areaCode[areaName];
+        }
+
+        private static IEnumerable<string> GetWeatherReportFromYahoo(int cityCode) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(
+                    @"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml", cityCode);
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes) {
+                    string s = Regex.Replace(node.Value, "【|】|- Yahoo!天気・災害", "");
+                    yield return s; //一件ずつデータを渡すことが出来る
+                }
+            }
+        }
+
     }
 }
