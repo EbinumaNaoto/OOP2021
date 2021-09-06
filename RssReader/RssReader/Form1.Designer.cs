5ce0011c0437e54d9e341538f55b38c2c9294f1c
尚人 海老沼 <32063@ojs.ac.jp>
2021-09-06 15:02:29 +0900

    タイトル表示完成

---
+++ RssReader/RssReader/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace RssReader {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.lbRss = new System.Windows.Forms.Label();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.lbTitles = new System.Windows.Forms.ListBox();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.btRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbRss
+            // 
+            this.lbRss.AutoSize = true;
+            this.lbRss.Font = new System.Drawing.Font("MS UI Gothic", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbRss.Location = new System.Drawing.Point(13, 13);
+            this.lbRss.Name = "lbRss";
+            this.lbRss.Size = new System.Drawing.Size(205, 40);
+            this.lbRss.TabIndex = 0;
+            this.lbRss.Text = "RssReader";
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbUrl.Location = new System.Drawing.Point(251, 18);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(883, 37);
+            this.tbUrl.TabIndex = 1;
+            // 
+            // lbTitles
+            // 
+            this.lbTitles.FormattingEnabled = true;
+            this.lbTitles.ItemHeight = 15;
+            this.lbTitles.Location = new System.Drawing.Point(20, 68);
+            this.lbTitles.Name = "lbTitles";
+            this.lbTitles.Size = new System.Drawing.Size(299, 709);
+            this.lbTitles.TabIndex = 2;
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Location = new System.Drawing.Point(325, 68);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.Size = new System.Drawing.Size(920, 709);
+            this.wbBrowser.TabIndex = 3;
+            // 
+            // btRead
+            // 
+            this.btRead.Location = new System.Drawing.Point(1140, 18);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(105, 37);
+            this.btRead.TabIndex = 4;
+            this.btRead.Text = "読込";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 789);
+            this.Controls.Add(this.btRead);
+            this.Controls.Add(this.wbBrowser);
+            this.Controls.Add(this.lbTitles);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.lbRss);
+            this.Name = "Form1";
+            this.Text = "32063";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbRss;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.ListBox lbTitles;
+        private System.Windows.Forms.WebBrowser wbBrowser;
+        private System.Windows.Forms.Button btRead;
+    }
+}
+
---
+++ RssReader/RssReader/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btRead_Click(object sender, EventArgs e) {
+
+            setRssTitle(tbUrl.Text);
+
+        }
+
+        //指定したURL先からXMLデータを取得しtitleを取得し、リストボックスへセットする
+        private void setRssTitle(string urlText) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(@"{0}",urlText);
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes) {
+                    string s = Regex.Replace(node.Value,"<title>", "");
+                    lbTitles.Items.Add(s);
+                }
+            }
+        }
+    }
+}
