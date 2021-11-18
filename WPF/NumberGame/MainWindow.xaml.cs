2c5f314cc7d4ea53ace634be6104d5677ab94704
尚人 海老沼 <32063@ojs.ac.jp>
2021-11-18 11:44:06 +0900

    神経衰弱画面配置完成

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,48 +21,62 @@ namespace NumberGame {
     /// </summary>
     public partial class MainWindow : Window {
 
+        private List<Button> buttons = new List<Button>();
+        private Random random = new Random();
+
         private int answerNumber { get; set; } //答えとなる数字
-        private int oneSide { get; set; } = 5;//一辺あたりのマス目
+        private int oneSide { get; set; } = 5; //一辺の長さ
 
         public MainWindow() {
             InitializeComponent();
-
-            SetNumberButton();
-            SetAnswerNumber();
         }
 
-        //ボタンを25個配置する処理
+        //ボタンを配置する処理
         private void SetNumberButton() {
+            //行列
+            for (int i = 0; i < oneSide; i++) {
+                gridPanel.RowDefinitions.Add(new RowDefinition());
+                gridPanel.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
             for (int i = 0; i < Math.Pow(oneSide,2); i++) {
                 Button button = new Button();
-                button.Content = (i+1).ToString();
-                button.Name = $"button{button.Content}";
+                button.Content = i+1;
                 button.Click += (sender, e) => Button_Click(sender);
-                button.Height = 50;
-                button.Width = 50;
+                button.Height = MainForm.Height/oneSide;
+                button.Width = MainForm.Width/oneSide;
                 button.FontSize = 30;
-                button.SetValue(Grid.RowProperty, i/oneSide); //行 割った数を使う
-                button.SetValue(Grid.ColumnProperty,i%oneSide); //列 余りを使う
-                gridPanel.Children.Add(button);
+                Grid.SetRow(button, i / oneSide); //行 割った数を使う
+                Grid.SetColumn(button, i % oneSide); //列 余りを使う
+                buttons.Add(button);
             }
+            buttons.ForEach(bt => gridPanel.Children.Add(bt));
+            MainForm.Height += answerText.Height + 50;
         }
 
         //答えとなる数字を生成する処理
         private void SetAnswerNumber() {
-            var random = new Random();
-            answerNumber = random.Next(1, (int)Math.Pow(oneSide, 2));
+            answerNumber = random.Next((int)Math.Pow(oneSide, 2))+1;
         }
 
         //ボタンがクリックされた時のイベントハンドラー
         private void Button_Click(object sender) {
             if (answerNumber < int.Parse(((Button)sender).Content.ToString())) {
-                answerLabel.Content = "小さい数です";
+                ((Button)sender).Background = Brushes.Gray;
+                answerText.Text = "小さい数です";
             } else if (answerNumber > int.Parse(((Button)sender).Content.ToString())) {
-                answerLabel.Content = "大きい数です";
+                ((Button)sender).Background = Brushes.Gray;
+                answerText.Text = "大きい数です";
             } else {
-                answerLabel.Content = "正解です";
+                ((Button)sender).Background = Brushes.Magenta;
+                answerText.Text = "正解です";
             }
             
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            SetAnswerNumber();
+            SetNumberButton();
         }
     }
 }
